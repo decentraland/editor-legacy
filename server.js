806c6fa913549f7437ceeca2d404267d008401d5
Esteban Ordano <esteban@decentraland.org>
2017-10-25 16:55:12 -0300

    Server-side events behind nginx

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ const sse = function (req, res, next) {
     res.writeHead(200, {
       'Content-Type': 'text/event-stream',
       'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive'
+      'Connection': '',
+      'X-Accel-Buffering': 'no'
     })
   }
 
